6ad83f56f2a902a2a2ca27c1711f7b77a966a480
Seth Thomas Rasmussen <sethrasmussen@gmail.com>
2010-01-26 17:36:16 -0500

    basic commit permalinking and loading

--- public/js/repo_manager.js
+++ public/js/repo_manager.js
@@ -5,5 +5,11 @@ $(function() {
     $.get("/diffs/" + sha, function(data) {
       $("#diffs").html(data)
     })
+    document.location.hash = sha
   })
+  
+  if (document.location.hash) {
+    var sha = document.location.hash.substring(1)
+    $("#commit_" + sha).click()
+  }
 })
