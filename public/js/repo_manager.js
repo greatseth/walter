4bb9641ca3194ce0e4239f1d512ac865d796ed75
Seth Thomas Rasmussen <sethrasmussen@gmail.com>
2010-01-26 17:51:11 -0500

    getting a little more grown up with our js

--- public/js/repo_manager.js
+++ public/js/repo_manager.js
@@ -1,16 +1,20 @@
-$(function() {
-  $("#commits li").click(function() {
+var RepoManager = {
+  onload: function() {
+    $("#commits li").click(RepoManager.get_diff)
+    
+    if (document.location.hash) {
+      var sha = document.location.hash.substring(1)
+      $("#commit_" + sha).click()
+    }
+  },
+  
+  get_diff: function() {
     var c    = $(this)
     var sha  = /[^_]+$/.exec(c.attr("id"))[0]
-    $.get("/diffs/" + sha, function(data) {
-      $("#diffs").html(data)
-    })
+    $.get("/diffs/" + sha, function(data) { $("#diffs").html(data) })
     document.location.hash = sha
     $("#sha").html(sha)
-  })
-  
-  if (document.location.hash) {
-    var sha = document.location.hash.substring(1)
-    $("#commit_" + sha).click()
   }
-})
+}
+
+$(RepoManager.onload)
