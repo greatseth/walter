16aa748ebb1911a7edf6324ecff614b29406c229
Seth Thomas Rasmussen <sethrasmussen@gmail.com>
2010-01-26 22:19:32 -0500

    only apply diffs overflow behavior when necessary, and do it correctly

--- public/js/repo_manager.js
+++ public/js/repo_manager.js
@@ -11,12 +11,14 @@ var RepoManager = {
   },
   
   get_diff: function() {
-    $("#diffs").html('<p class="diff_loading">loading...</p>')
+    $("#diffs").removeClass("hiding_overflow").html('<p class="diff_loading">loading...</p>')
     var c = $(this)
     $("#commits li.selected").removeClass("selected")
     c.addClass("selected")
     var sha = /[^_]+$/.exec(c.attr("id"))[0]
-    $.get("/diffs/" + sha, function(data) { $("#diffs").html(data) })
+    $.get("/diffs/" + sha, function(data) {
+      $("#diffs").addClass("hiding_overflow").html(data)
+    })
     document.location.hash = sha
     $("#sha").html(sha.substring(0,18) + "...")
   }
